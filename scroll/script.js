2e5f31e0f7b02082858cb49c904dfde9f3218ff5
sam-in95 <61958543+sam-in95@users.noreply.github.com>
2022-03-29 16:14:15 +0900

    fix btn opacity and button size

--- scroll/script.js
+++ scroll/script.js
@@ -1,5 +1,6 @@
 const $btns = document.querySelectorAll(".btn");
 const $contents = document.querySelectorAll(".contents");
+
 $btns.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     $contents[index].scrollIntoView({ behavior: "smooth", block: "center" });
@@ -9,10 +10,10 @@ $btns.forEach((btn, index) => {
 const $topBtn = document.querySelector(".top");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 60) {
-    $topBtn.style.opacity = "1";
+  if (window.scrollY > 300) {
+    $topBtn.classList.add("top_show");
   } else {
-    $topBtn.style.opacity = "0";
+    $topBtn.classList.remove("top_show");
   }
 });
 
