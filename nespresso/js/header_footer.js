d5e90d4652ac7c8e8942548426033b6ce6870e1d
sam-in95 <61958543+sam-in95@users.noreply.github.com>
2022-04-14 09:19:12 +0900

    finish_nespresso_0414_school_ver

--- nespresso/js/header_footer.js
+++ nespresso/js/header_footer.js
@@ -1,57 +1,79 @@
 //header esc button
 
-const $alertNav = document.querySelector(".alert_nav_wrap");
-const $escBtn = document.querySelector(".esc");
+const $alertNav = document.querySelector('.alert_nav_wrap');
+const $escBtn = document.querySelector('.esc');
 
-$escBtn.addEventListener("click", () => {
-  $alertNav.style.transform = "translateY(-50px)";
+$escBtn.addEventListener('click', () => {
+  $alertNav.style.transform = 'translateY(-50px)';
   setTimeout(() => {
-    $alertNav.style.display = "none";
+    $alertNav.style.display = 'none';
   }, 200);
 });
 
 // header button
-const $headerBtns = document.querySelectorAll(".login_btn");
+const $headerBtns = document.querySelectorAll('.header_btn');
+const $headerBtnsIcon = document.querySelectorAll('.header_btn .icon');
 
-$headerBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
+$headerBtns.forEach((btn, index) => {
+  btn.addEventListener('click', (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    // 클릭위치
 
-    const buttonTop = e.target.offsetTop;
-    const buttonLeft = e.target.offsetLeft;
-    // 버튼위치
+    const targetRect = btn.getBoundingClientRect();
 
-    const xInside = x - buttonLeft;
-    const yInside = y - buttonTop;
-    console.log(xInside, yInside);
-    // 버튼 내부 클릭위치
+    const buttonTop = y - targetRect.y;
+    const buttonLeft = x - targetRect.x;
 
-    const $circle = document.createElement("span");
-    $circle.classList.add("circle");
-    $circle.style.top = yInside + "px";
-    $circle.style.left = xInside + "px";
+    const $circle = document.createElement('span');
+    $circle.classList.add('circle');
+    $circle.style.top = buttonTop + 'px';
+    $circle.style.left = buttonLeft + 'px';
 
     e.target.appendChild($circle);
-    console.log(e.target);
-    // function함수에선 this가능
-    // 이벤트 위임..?
+
+    if (index === 0) {
+      // .header
+      // btn.style.color = '#000';
+      $circle.style.backgroundColor = '#fff';
+    } else {
+      // btn.style.color = '#000';
+      $circle.style.backgroundColor = '#c75b18';
+    }
+
+    setTimeout(() => {
+      $circle.remove();
+    }, 500);
   });
 });
 
 // nav
-const $navLi = document.querySelectorAll(".nav_wrap ul li");
-const $subNavBack = document.querySelector(".sub_nav_back");
-const $subNav = document.querySelectorAll(".sub_nav");
+const $navLi = document.querySelectorAll('.nav_wrap > ul > li');
+const $subNavBackWrap = document.querySelector('.sub_nav_back_wrap');
+const $subNavBack = document.querySelectorAll('.sub_nav_color');
+const $subNav = document.querySelectorAll('.sub_nav');
 
 $navLi.forEach((nav, index) => {
-  nav.addEventListener("mouseover", () => {
-    $subNavBack.style.display = "block";
-    $subNav.style.display = "block !important";
+  nav.addEventListener('mouseover', () => {
+    nav.classList.add('nav_now');
+    $subNavBackWrap.style.borderTop = '1px solid #f2f2f2';
+    $subNavBackWrap.style.height = '250px';
+    $subNavBack[index].style.backgroundColor = '#3e3e3e';
+    nav.style.color = '#d3391b';
+
+    $subNav.forEach((subNav) => {
+      subNav.style.display = 'block';
+    });
   });
 
-  nav.addEventListener("mouseleave", () => {
-    $subNavBack.style.display = "none";
+  nav.addEventListener('mouseleave', () => {
+    nav.classList.remove('nav_now');
+    $subNavBackWrap.style.borderTop = 'none';
+    $subNavBackWrap.style.height = '0';
+    $subNavBack[index].style.backgroundColor = '#333';
+    nav.style.color = '#fff';
+
+    $subNav.forEach((subNav) => {
+      subNav.style.display = 'none';
+    });
   });
 });
