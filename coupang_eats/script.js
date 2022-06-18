99e1206f1ad7a1d625a9868e54b8f7a0850e65d0
sam-in95 <qlrkqnf@naver.com>
2022-06-18 23:44:09 +0900

    fix coupang

--- coupang_eats/script.js
+++ coupang_eats/script.js
@@ -1,43 +1,36 @@
 const bike = document.querySelector('.banner_bike');
 
 window.addEventListener('load', () => {
-  bike.style.transition = '2s';
+  bike.style.transition = 'ease-in-out 2s';
   bike.style.transform = 'translate(500px, -35px)';
 });
 
+const iconLine = document.querySelector('.icon_line');
 const benefitCard = document.querySelectorAll('.benefit_card');
 const joinCard = document.querySelectorAll('.join_card');
 
 window.addEventListener('scroll', () => {
   let scrollTop = window.scrollY;
-  console.log(scrollTop);
+
+  if (scrollTop > 450) {
+    iconLine.style.transform = 'translateX(110px)';
+  }
+
   if (scrollTop > 1600) {
-    benefitCard[0].style.transform = 'translateY(0)';
-    benefitCard[0].style.opacity = '1';
-
-    setTimeout(() => {
-      benefitCard[1].style.transform = 'translateY(0)';
-      benefitCard[1].style.opacity = '1';
-    }, 100);
-
-    setTimeout(() => {
-      benefitCard[2].style.transform = 'translateY(0)';
-      benefitCard[2].style.opacity = '1';
-    }, 200);
+    benefitCard.forEach((card, index) => {
+      setTimeout(() => {
+        benefitCard[index].style.transform = 'translateY(0)';
+        benefitCard[index].style.opacity = '1';
+      }, index * 100);
+    });
   }
 
   if (scrollTop > 2420) {
-    joinCard[0].style.transform = 'translateY(0)';
-    joinCard[0].style.opacity = '1';
-
-    setTimeout(() => {
-      joinCard[1].style.transform = 'translateY(0)';
-      joinCard[1].style.opacity = '1';
-    }, 100);
-
-    setTimeout(() => {
-      joinCard[2].style.transform = 'translateY(0)';
-      joinCard[2].style.opacity = '1';
-    }, 200);
+    joinCard.forEach((card, index) => {
+      setTimeout(() => {
+        joinCard[index].style.transform = 'translateY(0)';
+        joinCard[index].style.opacity = '1';
+      }, index * 100);
+    });
   }
 });
